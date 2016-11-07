e76aac5547affd64be63a4b5327eb6ab853bfc9b
Daniel Kang <me@daniel.gs>
2016-11-07 11:03:08 -0800

    deploy resources

---
+++ Makefile
@@ -0,0 +1,13 @@
+BUILD_DIR := /tmp/tutorial-meteor
+
+bundle:
+	mkdir -p $(BUILD_DIR)
+	rm -rf $(BUILD_DIR)/bundle
+	meteor build --directory $(BUILD_DIR)/ --architecture os.linux.x86_64 --server http://localhost:3000 --server-only
+
+deploy:
+	cp -rf deploy/* $(BUILD_DIR)/
+	cp -rf coldbrew.conf $(BUILD_DIR)/
+	coldbrew deploy --app-dir $(BUILD_DIR) --dockerfile $(BUILD_DIR)/Dockerfile
+
+.PHONY: deploy bundle
---
+++ deploy/script/lib/install_phantomjs.sh
@@ -0,0 +1,15 @@
+# Install PhantomJS
+set -e
+apt-get -y install libfreetype6 libfreetype6-dev fontconfig
+ARCH=`uname -m`
+PHANTOMJS_VERSION=2.1.1
+PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+
+cd /usr/local/share/
+curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+tar xjf $PHANTOMJS_TAR_FILE
+ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
+ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
+ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/bin/phantomjs
+
+rm $PHANTOMJS_TAR_FILE
